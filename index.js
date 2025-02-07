92c8af2f61822593a6017eab66c82347edb375e6
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-02-07 11:58:02 +0530

    completed the assignment

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ app.get("/", (req, res) => {
 // Start the Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port https://localhost:${PORT}`));
+
+
+
+
+
